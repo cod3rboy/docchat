c97afa7148d8339aa04fa8d44d465d3925c93937
Dheeraj Kumar <dkchalotra@gmail.com>
2026-07-29 21:46:41 +0530

    implement thread panel ui

---
+++ frontend/src/components/ThreadFormPopover.tsx
@@ -0,0 +1,78 @@
+import { Button, Flex, Heading, Popover, TextField } from "@radix-ui/themes";
+import {
+  PropsWithChildren,
+  SubmitEventHandler,
+  useEffect,
+  useState,
+} from "react";
+
+export type Thread = {
+  id: string;
+  title: string;
+};
+
+export interface ThreadFormPopoverProps {
+  heading: string;
+  onSubmit: (thread: Thread) => void;
+  thread?: Thread;
+  clearable?: boolean;
+}
+
+export function ThreadFormPopover({
+  heading,
+  onSubmit,
+  children,
+  thread,
+  clearable = false,
+}: PropsWithChildren<ThreadFormPopoverProps>) {
+  const [threadTitle, setThreadTitle] = useState<string>(thread?.title ?? "");
+
+  useEffect(() => {
+    if (thread && !clearable) {
+      setThreadTitle(thread.title);
+    }
+  }, [thread]);
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (threadTitle.trim() === "") {
+      return;
+    }
+
+    const threadId = `${threadTitle}_${10000 + Math.trunc(Math.random() * 900000)}`;
+    onSubmit({
+      id: thread?.id ?? threadId,
+      title: threadTitle,
+    });
+
+    if (clearable) {
+      setThreadTitle("");
+    }
+  };
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger>{children}</Popover.Trigger>
+      <Popover.Content>
+        <Heading mb="2" size="2" weight="medium">
+          {heading}
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Flex gap="1">
+            <TextField.Root
+              value={threadTitle}
+              onInput={(e) => setThreadTitle(e.currentTarget.value)}
+              name="threadTitle"
+              placeholder="Thread title"
+            ></TextField.Root>
+            <Popover.Close>
+              <Button type="submit" disabled={threadTitle.trim() === ""}>
+                Start
+              </Button>
+            </Popover.Close>
+          </Flex>
+        </form>
+      </Popover.Content>
+    </Popover.Root>
+  );
+}
--- frontend/src/components/ThreadPanel.tsx
+++ frontend/src/components/ThreadPanel.tsx
@@ -1,9 +1,91 @@
-import { Container, Text } from "@radix-ui/themes";
+import {
+  ChatBubbleIcon,
+  DotsVerticalIcon,
+  PlusIcon,
+} from "@radix-ui/react-icons";
+import {
+  Flex,
+  Grid,
+  Heading,
+  IconButton,
+  ScrollArea,
+  Text,
+} from "@radix-ui/themes";
+import { ThreadFormPopover, type Thread } from "./ThreadFormPopover";
+
+const threads: Thread[] = [
+  {
+    id: "1",
+    title: "My Thread 1",
+  },
+  {
+    id: "2",
+    title: "My Thread 2",
+  },
+  {
+    id: "3",
+    title: "My Thread 3",
+  },
+  {
+    id: "4",
+    title: "My Thread 4",
+  },
+  {
+    id: "5",
+    title: "This is a very longggg title for the thread",
+  },
+];
 
 export function ThreadPanel() {
   return (
-    <Container style={{ background: "blue" }}>
-      <Text>Threads Panel</Text>
-    </Container>
+    <Grid columns="1" rows="auto 1fr" height="100vh" overflow="hidden">
+      <Flex
+        gap="2"
+        align="center"
+        justify="between"
+        p="2"
+        style={{ borderBottom: "1px solid var(--gray-6)" }}
+      >
+        <Flex gap="2">
+          <ChatBubbleIcon width="16" height="16" />
+          <Heading size="2" color="gray">
+            Threads
+          </Heading>
+        </Flex>
+        <ThreadFormPopover
+          heading="Start new thread"
+          onSubmit={() => {}}
+          clearable
+        >
+          <IconButton variant="ghost">
+            <PlusIcon width="16" height="16" />
+          </IconButton>
+        </ThreadFormPopover>
+      </Flex>
+      <ScrollArea size="1" scrollbars="vertical" type="hover" className="p-1">
+        {threads.map(({ id, title }) => (
+          <Grid
+            key={id}
+            columns="1fr auto"
+            gap="1"
+            p="2"
+            className="group cursor-default hover:bg-gray-100 hover:rounded-2xl"
+            align="center"
+          >
+            <Text size="2" weight="medium" truncate>
+              {title}
+            </Text>
+            <IconButton
+              variant="ghost"
+              color="gray"
+              radius="full"
+              style={{ background: "none" }}
+            >
+              <DotsVerticalIcon />
+            </IconButton>
+          </Grid>
+        ))}
+      </ScrollArea>
+    </Grid>
   );
 }
